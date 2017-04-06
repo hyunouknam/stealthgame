a07e079f74c322757644264642af9983d51ca7af
Nick Lombardi <Nick Lombardi>
2017-04-06 11:50:47 -0400

    Menus Fixed!

--- js/SplashScreen.js
+++ js/SplashScreen.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
+
 var game,eye_iris,eye_white,start,titleImage,logo,helpScreen,controlsScreen,
     levelSelectionButton,controlsButton,helpButton,mainMenuButton 
     fade = false;
 var currentState = "Splash Screen";
 
-=======
-var game;
-var eye_iris;
-var eye_white;
 var eye_scale = 4;
->>>>>>> refs/remotes/origin/master
 function init(){
     game = new Phaser.Game(1200,700,Phaser.AUTO,'',{preload: preload, create: create, update: update});
 }
@@ -33,7 +28,6 @@ function create(){
     game.stage.backgroundColor = "#444";
     titleImage = game.add.sprite(600,90,'title');
     titleImage.anchor.setTo(.5);
-<<<<<<< HEAD
     start = game.add.sprite(600,600,'start');
     start.anchor.setTo(.5);
     start.fade = true;
@@ -45,16 +39,10 @@ function create(){
     eye_iris.scale.setTo(5);
 
     game.input.mouse.capture = true;
-=======
-    eye_white = game.add.sprite(600,270,'eye_white')
-    eye_white.anchor.setTo(.5);
-    eye_iris = game.add.sprite(600,270,'eye_iris');
-    eye_iris.anchor.setTo(.5);
     
     game.stage.smoothed = false;
     eye_iris.scale.setTo(eye_scale);
     eye_white.scale.setTo(eye_scale);
->>>>>>> refs/remotes/origin/master
 }
 function update(){
     if(currentState == "Splash Screen"){
@@ -92,17 +80,12 @@ function eyeMovement(){
     var dx = mouseX-eye_white.x;
     var dy = mouseY-eye_white.y;
 
-<<<<<<< HEAD
-    if(dx*dx+dy*dy <= 6400){             // 225 is area using 15 below
-=======
-    if(dx*dx+dy*dy <= 225*eye_scale*eye_scale){             // 225 is area using 15 below
->>>>>>> refs/remotes/origin/master
+    if(dx*dx+dy*dy <= eye_scale*eye_scale*225){             // 225 is area using 15 below
         eye_iris.x = mouseX;
         eye_iris.y = mouseY;
     }else{
         if(dx*dx+dy*dy>30){ 
             var angle=Math.atan2(dy,dx);    //Get the angle
-<<<<<<< HEAD
             eye_iris.x = eye_white.x + 80 * Math.cos(angle);
             eye_iris.y = eye_white.y + 80 * Math.sin(angle);
         }
@@ -149,10 +132,6 @@ function titleScreenTransition(){
         }else{
             currentState = "Title Screen";
             fade = false;
-=======
-            eye_iris.x = eye_white.x + 15*eye_scale * Math.cos(angle);
-            eye_iris.y = eye_white.y + 15*eye_scale * Math.sin(angle);
->>>>>>> refs/remotes/origin/master
         }
     }
 }
