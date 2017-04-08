7191583851fb377fb1e2dc72676300d549baa34f
Nick Lombardi <Nick Lombardi>
2017-04-08 19:48:28 -0400

    In-game hud finished

--- js/play.js
+++ js/play.js
@@ -2,7 +2,7 @@ var health = 194;
 var stamina = 194;
 var sanity = 194;
 var healthBar,staminaBar,sanityBar,isPaused=false,
-    pausedMenu, locked;
+    pausedMenu, locked, resumeButton;
 var escKey;
 var playState = {
     preload: function(){
@@ -11,6 +11,7 @@ var playState = {
         game.load.image('stamina_bar','../assets/Stamina_Bar.png');
         game.load.image('sanity_bar','../assets/Sanity_Bar.png');
         game.load.image('paused_image','../assets/Paused_Menu.png');
+        game.load.image('resume_button','../assets/Resume_Button.png');
     },
     create: function(){
         game.add.sprite(0,550,'hud');
@@ -25,10 +26,18 @@ var playState = {
                 locked = true;
                 isPaused = true;
                 pausedMenu = game.add.sprite(0,0,'paused_image');
-                game.time.events.add(Phaser.Timer.SECOND*.2,function(){locked = false;});
+                resumeButton = game.add.button(425,130,'resume_button',function(){resumeButton.isPressed=true;resume()});
+                game.time.events.add(Phaser.Timer.SECOND,function(){locked = false;});
             }
         }else if(escKey.isDown && !locked){
-                game.time.events.add(Phaser.Timer.SECOND*.2,function(){pausedMenu.kill();isPaused = false;});
+               resume();
         }
+    }
+    
+}
+function resume(){
+    if((escKey.isDown && !locked) || (resumeButton.isPressed)){
+        resumeButton.isPressed = false;
+        game.time.events.add(Phaser.Timer.SECOND*.2,function(){pausedMenu.destroy();resumeButton.destroy();isPaused = false;}); 
     }
 }
